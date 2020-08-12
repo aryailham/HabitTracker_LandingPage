1fa666c3d9b3b691d8926d3c7ebd66bb32a81454
Arya <aryailham05@users.github.com>
2020-08-12 18:03:53 +0700

    Update slider.js

--- HabitTracker/js/slider.js
+++ HabitTracker/js/slider.js
@@ -1,4 +1,15 @@
-$(document).ready(function(){
+// $(document).ready(function(){
+//     $('.slider').bxSlider({
+//         mode: 'horizontal',
+//         pager: false,
+//         responsive: true,
+//         auto: false,
+//         infiniteLoop : false,
+//         hideControlOnEnd : true
+//     });
+// });
+
+$(window).on('load', function(){
     $('.slider').bxSlider({
         mode: 'horizontal',
         pager: false,
